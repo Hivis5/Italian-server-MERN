97af7077ebbe6360890732fdfca14771f4205244
hitesh vishal <56801446+Hivis5@users.noreply.github.com>
2022-03-30 12:49:51 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,4 +1,4 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const dbUrl = "mongodb+srv://Hivis:1234@hitesh.fatyu.mongodb.net/test";
+const dbUrl = "mongodb+srv://Hivis:<password>@hitesh.fatyu.mongodb.net/test";
 module.exports = { dbUrl, mongodb, MongoClient };
